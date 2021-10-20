dde532be73e3230e167419d1d6ab09791b6c9cec
Multiwebinc <multiwebinc@users.noreply.github.com>
2021-10-19 19:47:46 -0600

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'fdb688c8b087a5c1061c23207795b56e'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'eca02817-bbda-48c1-8bf9-6ed274dbad14'
+};
